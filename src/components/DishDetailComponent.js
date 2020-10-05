d1937811fd2d7b05dc2254a0eb2266c13369b73a
Alaeddinehnana <alaeddine.hnana@esprit.tn>
2020-10-05 15:42:43 +0100

    Fetch From Server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -18,6 +18,7 @@ import {
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 function RenderComments({comments, addComment, dishId}) {
     if (comments == null) {
@@ -56,7 +57,7 @@ function RenderComments({comments, addComment, dishId}) {
       return (
           <div  className="col-12 col-md-5 m-1">
             <Card>
-              <CardImg width="100%"  src={dish.image} alt={dish.name}/>
+              <CardImg width="100%"  src={baseUrl + dish.image} alt={dish.name}/>
               <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
